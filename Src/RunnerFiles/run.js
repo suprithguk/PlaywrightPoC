699b5137bf8a539e44ac1d8595c76413cda4a15f
suprithguk <suprithguk@gmail.com>
2024-06-27 12:25:41 +0100

    Create run.js

---
+++ Src/RunnerFiles/run.js
@@ -0,0 +1,13 @@
+const runner = {
+  ENV = "",
+  BROWSER = "",
+  PACK: "", 
+  SUB_PACK: "",
+  SUB_TO_SUB_PACK: "", 
+  FEATURE: "",
+  TAGS: "",
+  VIDEORECORDER: "", 
+  TRACEVIEWER: ""
+}
+
+  module.exports = {runner}
